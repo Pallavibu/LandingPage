37fcd01e8529205cf5d7a21c12d9cae3b91cb214
Pallavibu <pallavibu44@gmail.com>
2024-01-05 12:29:33 +0530

    done with task 1

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,12 +1,12 @@
 import profile from "../Assets/profile.jpg";
-import addline from "../Assets/addline.jpg"
-import mobile from "../Assets/mobile.jpg"
+import addline from "../Assets/addline.jpg";
+import mobile from "../Assets/mobile.jpg";
 
 const Body = () => {
   return (
     <div className=" bg-blue-200 px-8 py-4">
-      <div className="flex gap-4 border border-green-950  bg-blue-800">
-        <div className="box1 w-1/4 flex flex-col gap-2">
+      <div className="flex gap-1 ">
+        <div className="box1 w-1/4 flex flex-col gap-1">
           <div className="flex h-28  rounded-md w-80 bg-gradient-to-r from-blue-200 via-orange-100 to-orange-200">
             <div className="h-12">
               <img src={profile} alt="profile" className="ml-5 mt-6" />
@@ -43,27 +43,30 @@ const Body = () => {
             </div>
           </div>
         </div>
-        <div className="box2 w-3/4 flex flex-col gap-3">
-          <div>
-            <div>
-                <text>My Reviews</text>
+        <div className="box2 w-3/4 flex flex-col gap-3 bg-white rounded-md">
+          <div className="flex justify-between mx-8 mt-6">
+            <div className="">
+              <text className="text-2xl font-semibold">My Reviews</text>
             </div>
             <div className="flex">
-                <img src={addline} alt="addline"/>
-                <input type="text" placeholder="text"></input>
+              <img src={addline} alt="addline" />
             </div>
           </div>
-          <div>
-            <div>
-                <img src={mobile} alt="mobile"/>
+          <div className="flex flex-col mt-16 items-center justify-center">
+            <div className="">
+              <img src={mobile} alt="mobile" />
             </div>
-            <div>
-              <div>
-                <text>OOPS!!</text>
-                <p>YOU HAVE NOT RATED ANYTHING YET</p>
+            <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center">
+                <div className="m-2 mt-4">
+                  <p className="text-2xl font-bold">OPPS!!</p>
+                </div>
+                <div>
+                  <p className="text-2xl font-bold">YOU'VE NOT RATED ANYTHING YET</p>
+                </div>
               </div>
-              <div>
-                <button>Browse now</button>
+              <div className="m-4 border-2 border-blue-900 bg-blue-200 rounded-full">
+                <button className="mx-5 my-2 text-blue-900 font-medium">Browse now</button>
               </div>
             </div>
           </div>
